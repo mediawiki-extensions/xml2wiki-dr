55082e8176fd4aeaa850e420fb76c62f566b2bda
daemonraco <daemonraco@b1e99507-c906-d96d-dd38-1d6846cbd51f>
2010-07-10 04:57:21 +0000

    * Translator supports attribute translations. * Attributes have prefix and suffixes.

--- trunk/config.php
+++ trunk/config.php
@@ -1,6 +1,41 @@
 <?php
 /**
+* Arrays.
+*/
+if(!isset($wgXML2WikiAllowdPaths)) {
+	$wgXML2WikiAllowdPaths = array();
+}
+if(!isset($wgXML2WikiConfig)) {
+	$wgXML2WikiConfig = array();
+}
+
+/**
 * Allowed Directories.
 */
 $wgXML2WikiAllowdPaths[] = dirname(__FILE__).DIRECTORY_SEPARATOR.'xmls';
+
+/**
+* Configuration.
+* @{
+*/
+/**
+* Information
+*	@{
+*/
+$wgXML2WikiConfig['showallowpaths'] = true;
+$wgXML2WikiConfig['showinstalldir'] = true;
+$wgXML2WikiConfig['showsysinfo']    = true;
+$wgXML2WikiConfig['showmodules']    = true;
+/**	@} */
+
+/**
+* Messages
+*	@{
+*/
+$wgXML2WikiConfig['attributesprefix']      = '@';
+$wgXML2WikiConfig['attributessuffix']      = '@';
+$wgXML2WikiConfig['transattributesprefix'] = '@';
+$wgXML2WikiConfig['transattributessuffix'] = '';
+/**	@} */
+/** @} */
 ?>
