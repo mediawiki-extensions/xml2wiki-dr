367636f7f448f3ea6079229ff22ac2554fcc68c1
daemonraco <daemonraco@b1e99507-c906-d96d-dd38-1d6846cbd51f>
2010-07-11 04:13:18 +0000

    * Adding special page Special:Xml2Wiki. * If an unknown wiki-file is requiered it proviedes a link to upload it. * Adding more internacionalization messages. * Security: 	* '?modules' eliminated. 	* '?info' eliminated.

--- trunk/xml2wiki-dr.php
+++ trunk/xml2wiki-dr.php
@@ -18,13 +18,7 @@ function Xml2Wiki_Hooker() {
 }
 
 if(!defined('MEDIAWIKI')) {
-	if(isset($_REQUEST['modules'])) {
-		Xml2Wiki::Instance()->modulesCheck();
-	} elseif(isset($_REQUEST['info'])) {
-		Xml2Wiki::Instance()->showInfo();
-	} else {
-		die();
-	}
+	die();
 } else {
 	/**
 	 * MediaWiki Extension hooks Setter.
@@ -32,11 +26,24 @@ if(!defined('MEDIAWIKI')) {
 	$wgExtensionFunctions[]               = 'Xml2Wiki_Hooker';
 	$wgExtensionMessagesFiles['xml2wiki'] = dirname( __FILE__ ).DIRECTORY_SEPARATOR.'xml2wiki-dr.i18n.php';
 
-
+	$wgAutoloadClasses['xml2wiki']   = dirname( __FILE__ ).DIRECTORY_SEPARATOR.'xml2wiki-dr.body.php'; # Tell MediaWiki to load the extension body.
+//	$wgExtensionAliasesFiles['xml2wiki'] = dirname( __FILE__ ).DIRECTORY_SEPARATOR.'xml2wiki-dr.alias.php';
+	$wgSpecialPages['xml2wiki']      = 'xml2wiki'; # Let MediaWiki know about your new special page.
+	$wgSpecialPageGroups['xml2wiki'] = 'other';
+	
 	/**
 	 * MediaWiki Extension Description.
 	 */
 	$wgExtensionCredits['parserhook'][] = array(
+		'name'            => Xml2Wiki::Property('name'),
+		'version'         => Xml2Wiki::Property('version'),
+		'date'            => Xml2Wiki::Property('date'),
+		'description'     => Xml2Wiki::Property('description'),
+		'descriptionmsg'  => Xml2Wiki::Property('descriptionmsg'),
+		'author'          => Xml2Wiki::Property('author'),
+		'url'             => Xml2Wiki::Property('url'),
+	);
+	$wgExtensionCredits['specialpage'][] = array(
 		'name'            => Xml2Wiki::Property('name'),
 		'version'         => Xml2Wiki::Property('version'),
 		'date'            => Xml2Wiki::Property('date'),
